2ea115065c0f369c82cedf1015768c6e0620985b
zzzeq <yangyujiang43@gmail.com>
2026-08-24 22:52:39 +0800

    update format

--- Assignment0/homework0.cpp
+++ Assignment0/homework0.cpp
@@ -4,25 +4,23 @@
 #include<iostream>
 
 int main() {
-    // 文件内容说明：
-    // 1. 给定一个点P，在齐次坐标下给出平移和旋转之后的结果
+    // 文件内容说明：给定一个点P，在齐次坐标下给出平移和旋转之后的结果
 
-    // 01 基本变量定义
+    // 1. 基本变量定义
     Eigen::Vector3f P(2.0f, 1.0f, 1.0f);
     float pi = acos(-1);
     float theta = 45.0 / 180.0 * pi;
 
-    // 02 M_rotation & M_translation
+    // 2. M_rotation & M_translation
     Eigen::Matrix3f M_rotation,M_translation;
     M_rotation << cos(theta), -sin(theta), 0.0f,
-        sin(theta), cos(theta), 0.0f,
-        0.0f, 0.0f, 1.0f;
+                  sin(theta), cos(theta), 0.0f,
+                  0.0f, 0.0f, 1.0f;
     M_translation << 1.0f, 0.0f, 1.0f,
-        0.0f, 1.0f, 2.0f,
-        0.0f, 0.0f, 1.0f;
+                     0.0f, 1.0f, 2.0f,
+                     0.0f, 0.0f, 1.0f;
 
-   // 03 P_after_transformation -> 
-
+   // 3. after_transformation P_prime
     Eigen::Vector3f P_prime = M_translation * M_rotation * P;
 
     std::cout << "P_after_transformation: \n ";
