a2b7df2cbe133714a3a6d8feba67606d9e0e9415
zzzeq <yangyujiang43@gmail.com>
2026-09-21 23:11:32 +0800

    Fix assignment 1

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -21,14 +21,14 @@ Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
 
 Eigen::Matrix4f get_model_matrix(float rotation_angle)
 {
-    // 定义绕z轴旋转变换矩阵
+    // Matrix Definition
     Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
     Eigen::Matrix4f translate;
 
-    // 弧度制计算
+    // radiation conversion
     float radius = rotation_angle * MY_PI / 180.0;
 
-    // 计算旋转变换矩阵
+    // Rotation matrix calculation
     translate << cos(radius), -sin(radius), 0.0f, 0.0f,
         sin(radius), cos(radius), 0.0f, 0.0f,
         0.0f, 0.0f, 1.0f, 0.0f,
@@ -41,21 +41,21 @@ Eigen::Matrix4f get_model_matrix(float rotation_angle)
 Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
                                       float z_near, float z_far)
 {
-    // 定义透视与投影变换矩阵
+    // Matrix Definition
     Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
     Eigen::Matrix4f persp_to_ortho = Eigen::Matrix4f::Identity();
     Eigen::Matrix4f orthoscale = Eigen::Matrix4f::Identity();
     Eigen::Matrix4f orthopos = Eigen::Matrix4f::Identity();
     Eigen::Matrix4f rotate = Eigen::Matrix4f::Identity();
 
-    // 立方体投影相关变量
+    // Cubic Variable
     float theta = eye_fov / 180 * MY_PI;
     float t = z_near * tan(theta/2) ;
     float r = t * aspect_ratio;
     float l = -r;
     float b = -t;
 
-    // 计算投影变换矩阵
+    // Projection Matrix Calculation
     persp_to_ortho << z_near, 0, 0, 0,
         0, z_near, 0, 0,
         0, 0, z_near + z_far, -z_near * z_far,
@@ -66,7 +66,7 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
         0, 0, 0, 1;
     orthoscale << 2 / (r - l), 0, 0, 0,
         0, 2 / (t - b), 0, 0,
-        0, 0, 2 / (z_near - z_far), 0,
+        0, 0, 2 / (z_far - z_near), 0,
         0, 0, 0, 1;
     rotate << 1, 0, 0, 0,
         0, 1, 0, 0,
@@ -82,19 +82,19 @@ Eigen::Matrix4f get_rotation(Vector3f axis, float angle)
     Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
     Eigen::Matrix3f rotation;
     
-    // 将任意轴向量转变为方向向量
+    // normalization
     Eigen::Vector3f normal_axis = axis.normalized();
 
-    // 将角度转变为弧度制
+    // radius conversion
     float radius = angle / 180.0f * MY_PI;
 
-    // 构建辅助矩阵N
+    // Auxiliary matrix
     Eigen::Matrix3f N;
     N << 0, -normal_axis[2], normal_axis[1],
         normal_axis[2], 0, -normal_axis[0],
         -normal_axis[1], normal_axis[0], 0;
 
-    // 建立罗德里格旋转矩阵
+    // Rodrigues' Rotation Formula
     Eigen::Matrix3f mat1 = cos(radius) * Eigen::Matrix3f::Identity();
     Eigen::Matrix3f mat2 = (1 - cos(radius)) * normal_axis * normal_axis.transpose();
     Eigen::Matrix3f mat3 = sin(radius) * N;
